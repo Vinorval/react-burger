f8c1e9fc80d8230332ea72d493de82d01acf225c
Vinorval <vinorval@yandex.ru>
2021-10-13 15:31:28 +0300

    fix: переписать компонент AppHeader на функциональный

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -2,21 +2,19 @@ import React from "react";
 import appHeaderStyles from './appHeader.module.css'
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
-class AppHeader extends React.Component {
-    render() {
-        return(
-            <header className={appHeaderStyles.header}>
-                <nav className={appHeaderStyles.navigation}>
-                    <div className={appHeaderStyles.navigation__conteiner}>
-                        <p className={`${appHeaderStyles.text} ${appHeaderStyles.text_type_first}`}><BurgerIcon type="primary" /> Конструктор</p>
-                        <p className={appHeaderStyles.text}><ListIcon type="secondary" /> Лента заказов</p>
-                    </div>
-                    <Logo/>
-                    <p className={appHeaderStyles.text}><ProfileIcon type="secondary" /> Личный кабинет</p>
-                </nav>
-            </header>
-        );
-    }
+function AppHeader() {
+    return(
+        <header className={appHeaderStyles.header}>
+            <nav className={appHeaderStyles.navigation}>
+                <div className={appHeaderStyles.navigation__conteiner}>
+                    <p className={`${appHeaderStyles.text} ${appHeaderStyles.text_type_first}`}><BurgerIcon type="primary" /> Конструктор</p>
+                    <p className={appHeaderStyles.text}><ListIcon type="secondary" /> Лента заказов</p>
+                </div>
+                <Logo/>
+                <p className={appHeaderStyles.text}><ProfileIcon type="secondary" /> Личный кабинет</p>
+            </nav>
+        </header>
+    );
 }
 
 export default AppHeader
